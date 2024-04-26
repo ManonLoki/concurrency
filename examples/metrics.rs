1b1b00caa5d2a88ba7b10f1695d254622032150c
罗超然 <manonloki@163.com>
2024-04-26 22:32:13 +0800

    features: support dashmap

--- examples/metrics.rs
+++ examples/metrics.rs
@@ -11,11 +11,11 @@ fn main() -> Result<()> {
     let metrics = Metrics::new();
 
     for i in 0..M {
-        task_work(i, metrics.clone())?;
+        task_work(i, metrics.clone());
     }
 
     for _ in 0..N {
-        request_work(metrics.clone())?;
+        request_work(metrics.clone());
     }
 
     loop {
@@ -24,33 +24,25 @@ fn main() -> Result<()> {
     }
 }
 
-fn task_work(idx: usize, metircs: Metrics) -> Result<()> {
+fn task_work(idx: usize, metircs: Metrics) {
     std::thread::spawn(move || {
         loop {
             let mut rng = rand::thread_rng();
             std::thread::sleep(std::time::Duration::from_millis(rng.gen_range(500..3000)));
             // 添加计数
-            metircs.inc(format!("thread.worker.{}", idx))?;
+            metircs.inc(format!("thread.worker.{}", idx));
         }
-        #[allow(unreachable_code)]
-        Ok::<_, anyhow::Error>(())
     });
-
-    Ok(())
 }
 
-fn request_work(metircs: Metrics) -> Result<()> {
+fn request_work(metircs: Metrics) {
     std::thread::spawn(move || {
         loop {
             let mut rng = rand::thread_rng();
             std::thread::sleep(std::time::Duration::from_millis(rng.gen_range(300..5000)));
             // 添加计数
             let req_idx = rng.gen_range(1..5);
-            metircs.inc(format!("req.page.{}", req_idx)).ok();
+            metircs.inc(format!("req.page.{}", req_idx));
         }
-        #[allow(unreachable_code)]
-        Ok::<_, anyhow::Error>(())
     });
-
-    Ok(())
 }
--- src/mertics.rs
+++ src/mertics.rs
@@ -1,59 +1,38 @@
-use anyhow::Result;
-use std::collections::HashMap;
+use dashmap::DashMap;
 use std::default::Default;
 use std::fmt::Display;
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
 /// 追踪信息 实现 inc,dec,snapshot
 /// 这里的Clone调用了Data的Clon 相当于 Metrics{ data: data.clone() }
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, u64>>>,
+    data: Arc<DashMap<String, u64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Self {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
     /// +1
-    pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self
-            .data
-            .write()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        let count = data.entry(key.into()).or_insert(0);
+    pub fn inc(&self, key: impl Into<String>) {
+        let mut count = self.data.entry(key.into()).or_insert(0);
         *count += 1;
-
-        Ok(())
     }
 
     /// -1
-    pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self
-            .data
-            .write()
-            .map_err(|e| anyhow::anyhow!(e.to_string()))?;
-
-        let count = data.entry(key.into()).or_insert(0);
+    pub fn dec(&self, key: impl Into<String>) {
+        let mut count = self.data.entry(key.into()).or_insert(0);
         *count -= 1;
-
-        Ok(())
-    }
-
-    /// 获取快照
-    pub fn snapshot(&self) -> HashMap<String, u64> {
-        self.data.read().unwrap().clone()
     }
 }
 
 impl Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.data.read().map_err(|_| std::fmt::Error {})?;
-        for (key, value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?;
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?;
         }
         Ok(())
     }
